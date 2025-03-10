da8ceb1c1dcdb4193c32cb25c83f6a5211c95e57
Ganten Xu <zjxu97@gmail.com>
2025-03-10 19:57:10 +0800

    add notice

--- src/main/java/com/ganten/tethys/leetcode75/N450.java
+++ src/main/java/com/ganten/tethys/leetcode75/N450.java
@@ -2,7 +2,53 @@ package com.ganten.tethys.leetcode75;
 
 import com.ganten.tethys.TreeNode;
 
+// TODO: 需要复习
 public class N450 {
+    /*
+                       15
+                     /    \
+                   7       24
+                /     \
+              3        10
+             /  \     /   \
+            2    4   8    12
+      0. 如果删除 7
+
+                       15
+                     /    \
+                   [7]       24
+                /     \
+              3        10
+             /  \     /   \
+            2    4   8    12
+      1.  最终找到 rightSmallest = 8
+      2.    将 7 的左边接到 8 的左边
+
+                       15
+                     /    \
+                  [7]       24
+                 /    \
+            [          10        ]
+            [         /   \      ]
+            [        8    12     ]
+            [       /            ]
+            [     3              ]
+            [    /  \            ]
+            [   2    4           ]
+
+       3.  然后将 10 的整个下面，代替 7
+
+                       15
+                     /    \
+        [          10       24    ]
+        [         /   \           ]
+        [        8    12          ]
+        [       /                 ]
+        [     3                   ]
+        [    /  \                 ]
+        [   2    4                ]
+     */
+    // 递归实现的步骤 3
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
 
@@ -16,9 +62,11 @@ public class N450 {
             if (root.right == null) return root.left;
 
             TreeNode rightSmallest = root.right;
+            // 步骤 1
             while (rightSmallest.left != null) {
                 rightSmallest = rightSmallest.left;
             }
+            // 步骤 2
             rightSmallest.left = root.left;
             return root.right;
         }
